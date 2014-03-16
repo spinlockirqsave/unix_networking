8593b340d1c46e4f53a41f36ef1c0e91005936a8
cf16 <cf16rocket@gmail.com>
2014-03-16 15:26:26 +0100

    flush the stdout

--- unpv13e/intro/daytimetcpcli.c
+++ unpv13e/intro/daytimetcpcli.c
@@ -1,34 +1,45 @@
 #include	"unp.h"
 
 int
-main(int argc, char **argv)
-{
-	int					sockfd, n;
-	char				recvline[MAXLINE + 1];
-	struct sockaddr_in	servaddr;
+main(int argc, char **argv) {
+    int sockfd, n;
+    char recvline[MAXLINE + 1];
+    struct sockaddr_in servaddr;
 
-	if (argc != 2)
-		err_quit("usage: a.out <IPaddress>");
+    if (argc != 2)
+        err_quit("usage: a.out <IPaddress>");
 
-	if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-		err_sys("socket error");
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+        err_sys("socket error");
 
-	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_port   = htons(13);	/* daytime server */
-	if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
-		err_quit("inet_pton error for %s", argv[1]);
+    bzero(&servaddr, sizeof (servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(13); /* daytime server */
+    if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
+        err_quit("inet_pton error for %s", argv[1]);
 
-	if (connect(sockfd, (SA *) &servaddr, sizeof(servaddr)) < 0)
-		err_sys("connect error");
+    if (connect(sockfd, (SA *) & servaddr, sizeof (servaddr)) < 0)
+        err_sys("connect error");
 
-	while ( (n = read(sockfd, recvline, MAXLINE)) > 0) {
-		recvline[n] = 0;	/* null terminate */
-		if (fputs(recvline, stdout) == EOF)
-			err_sys("fputs error");
-	}
-	if (n < 0)
-		err_sys("read error");
+    while ((n = read(sockfd, recvline, MAXLINE)) > 0) {
+        recvline[n] = 0; /* null terminate */
+        //printf( "%s", recvline);
+        if (fputs(recvline, stdout) == EOF)
+            err_sys("fputs error");
+        /*
+        Since the stream is fully-buffered by default, not line-buffered,
+        it needs to be flushed periodically.  We'll flush it here for
+        demonstration purposes, even though we're about to close it.
+         */
+        int flush_status = fflush(stdout);
+        if ( flush_status != 0) {
+            puts("\nError flushing stream!");
+        } else {
+            puts("\nStream flushed.");
+        }
+    }
+    if (n < 0)
+        err_sys("read error");
 
-	exit(0);
+    exit(0);
 }
