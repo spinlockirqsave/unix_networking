015786dbaa30bac7be940cbcfd90233c1c5fcee1
cf16 <cf16rocket@gmail.com>
2014-05-09 00:52:06 +0200

    tabs

--- unpv13e/select/tcpcli01.c
+++ unpv13e/select/tcpcli01.c
@@ -4,18 +4,18 @@
 int
 main(int argc, char **argv)
 {
-	int					sockfd;
+	int			sockfd;
 	struct sockaddr_in	servaddr;
 
-	if (argc != 2)
-		err_quit("usage: tcpcli <IPaddress>");
+	if ( argc != 2)
+		err_quit( "usage: tcpcli <IPaddress>");
 
-	sockfd = Socket(AF_INET, SOCK_STREAM, 0);
+	sockfd = Socket( AF_INET, SOCK_STREAM, 0);
 
-	bzero(&servaddr, sizeof(servaddr));
+	bzero( &servaddr, sizeof( servaddr));
 	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(7);
-	Inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+	servaddr.sin_port = htons( SERV_PORT);
+	Inet_pton( AF_INET, argv[1], &servaddr.sin_addr);
 
 	Connect(sockfd, (SA *) &servaddr, sizeof(servaddr));
 
